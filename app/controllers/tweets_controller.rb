764abecd40eb3da29319cd78074e3382638a0a0f
youki0607 <its_showtime@i.softbank.jp>
2020-09-22 00:32:21 +0900

    active_storage導入

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,10 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:image.tweet_text).merge(user_id: current_user.id)
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  has_one_attached :image
 end
