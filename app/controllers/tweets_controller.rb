f6360e0de23b2611ef3b948f6d39244d7f3d31d6
youki0607 <its_showtime@i.softbank.jp>
2020-09-22 12:31:40 +0900

    tweet controller追記

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,45 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = Tweet.all
   end
 
+  def new
+    @tweet = Tweet.new 
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
 
   def tweet_params
-    params.require(:tweet).permit(:image.tweet_text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:name, :image, :tweet_text)#.merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
